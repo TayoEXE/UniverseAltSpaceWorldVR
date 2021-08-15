de73d67f5200de9a616d461e3a674f9fa03e190a
TayoEXE <rockmantaiyo@hotmail.com>
2021-08-14 19:58:49 -0700

    First commit

---
+++ Test World/Assets/Plugins/signin.bat
@@ -0,0 +1,3 @@
+echo off
+title Sign In to AltspaceVR
+curl -v -d "user[email]=rockmantaiyo@hotmail.com&user[password]=Gyakuten24" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ Test World/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@D:\New Folder 6\Unity\Introduction to Unity in 3DVR\Create with Code\Prototype 4\testing 2019.4 world\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1793892839130661131.json
